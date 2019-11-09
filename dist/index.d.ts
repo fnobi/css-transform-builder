2ea5af813242302df297b2aac80da1e791afe332
circleci <circleci@example.com>
2019-11-09 16:57:26 +0000

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,26 @@
-export declare function groupBy<T, P>(arr: T[], identifier: (val: T) => P): Map<P, T[]>;
-export declare const padLeft: (num: number, count: number, glue?: string) => string;
+export default class CSSTransformBuilder {
+    private readonly queue;
+    constructor(queue?: string[]);
+    private addOperation;
+    private addOperationNumbers;
+    scale(x: number, y: number): CSSTransformBuilder;
+    scaleX(x: number): CSSTransformBuilder;
+    scaleY(y: number): CSSTransformBuilder;
+    scaleZ(z: number): CSSTransformBuilder;
+    scale3d(x: number, y: number, z: number): CSSTransformBuilder;
+    translate(x: number, y: number): CSSTransformBuilder;
+    translateX(num: number): CSSTransformBuilder;
+    translateY(num: number): CSSTransformBuilder;
+    translateZ(num: number): CSSTransformBuilder;
+    translate3d(x: number, y: number, z: number): CSSTransformBuilder;
+    rotate(num: number): CSSTransformBuilder;
+    rotate3d(x: number, y: number, z: number, deg: number): CSSTransformBuilder;
+    rotateX(num: number): CSSTransformBuilder;
+    rotateY(num: number): CSSTransformBuilder;
+    rotateZ(num: number): CSSTransformBuilder;
+    skew(x: number, y: number): CSSTransformBuilder;
+    skewX(num: number): CSSTransformBuilder;
+    skewY(num: number): CSSTransformBuilder;
+    perspective(num: number): CSSTransformBuilder;
+    toString(): string;
+}
--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,77 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function groupBy(arr, identifier) {
-    const m = new Map();
-    arr.forEach(item => {
-        const prop = identifier(item);
-        const collection = m.get(prop);
-        if (collection) {
-            collection.push(item);
-        }
-        else {
-            m.set(prop, [item]);
-        }
-    });
-    return m;
+class CSSTransformBuilder {
+    constructor(queue = []) {
+        this.queue = queue;
+    }
+    addOperation(fn, val) {
+        return new CSSTransformBuilder([...this.queue, `${fn}(${val})`]);
+    }
+    addOperationNumbers(fn, nums, unit = "") {
+        return this.addOperation(fn, nums.map(n => `${n}${unit}`).join(","));
+    }
+    // matrix(数値, 数値, 数値, 数値, 数値, 数値)
+    // matrix3d(数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値, 数値)
+    scale(x, y) {
+        return this.addOperationNumbers("scale", [x, y]);
+    }
+    scaleX(x) {
+        return this.addOperationNumbers("scaleX", [x]);
+    }
+    scaleY(y) {
+        return this.addOperationNumbers("scaleY", [y]);
+    }
+    scaleZ(z) {
+        return this.addOperationNumbers("scaleZ", [z]);
+    }
+    scale3d(x, y, z) {
+        return this.addOperationNumbers("scale3d", [x, y, z]);
+    }
+    translate(x, y) {
+        return this.addOperationNumbers("translate", [x, y], "px");
+    }
+    translateX(num) {
+        return this.addOperationNumbers("translateX", [num], "px");
+    }
+    translateY(num) {
+        return this.addOperationNumbers("translateY", [num], "px");
+    }
+    translateZ(num) {
+        return this.addOperationNumbers("translateZ", [num], "px");
+    }
+    translate3d(x, y, z) {
+        return this.addOperationNumbers("translate3d", [x, y, z], "px");
+    }
+    rotate(num) {
+        return this.addOperationNumbers("rotate", [num], "deg");
+    }
+    rotate3d(x, y, z, deg) {
+        return this.addOperation("rotate3d", [x, y, z, `${deg}deg`].join(","));
+    }
+    rotateX(num) {
+        return this.addOperationNumbers("rotateX", [num], "deg");
+    }
+    rotateY(num) {
+        return this.addOperationNumbers("rotateY", [num], "deg");
+    }
+    rotateZ(num) {
+        return this.addOperationNumbers("rotateZ", [num], "deg");
+    }
+    skew(x, y) {
+        return this.addOperationNumbers("skew", [x, y], "deg");
+    }
+    skewX(num) {
+        return this.addOperationNumbers("skewX", [num], "deg");
+    }
+    skewY(num) {
+        return this.addOperationNumbers("skewY", [num], "deg");
+    }
+    perspective(num) {
+        return this.addOperationNumbers("perspective", [num]);
+    }
+    toString() {
+        return this.queue.length ? this.queue.join(" ") : "none";
+    }
 }
-exports.groupBy = groupBy;
-exports.padLeft = (num, count, glue = '0') => {
-    const str = num.toString();
-    if (str.length >= count)
-        return str;
-    return (new Array(count).join(glue) + str).slice(-count);
-};
+exports.default = CSSTransformBuilder;
 //# sourceMappingURL=index.js.map
